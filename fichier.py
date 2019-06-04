7e5dd631f9061fd91c75f23c4cf6382b9d30f50a
emilie <bout.emilie1@gmail.com>
2019-06-04 10:40:37 +0200

    add ia test with iris

---
+++ fichier.py
@@ -0,0 +1,18 @@
+import csv
+
+
+with open('test.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow(['John Smith', 'Accounting', 'November'])
+    employee_writer.writerow(['Erica Meyers', 'IT', 'March'])
+
+
+
+def normalize(df):
+    result = df.copy()
+    for feature_name in df.columns:
+        max_value = df[feature_name].max()
+        min_value = df[feature_name].min()
+        result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
+    return result
